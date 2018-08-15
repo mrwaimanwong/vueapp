ec9466be39c8844b4694e2eb228ea6971825f42a
mrwaimanwong <hello@waimanwong.com>
2018-08-14 17:51:20 -0700

    Successfully connected to WooCommerce Rest API (v2).

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,42 @@
 <template>
-  <div class="hello">
+<div class="hello">
 
-  <div v-for="proj in projects">
-    <h1>{{ proj.title}}</h1>
+  <div v-for="prod in products">
+    <h1>{{ prod.name}}</h1>
   </div>
+  <!-- <div v-for="proj in projects">
+    <h1>{{ proj.title}}</h1>
+  </div> -->
 </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App',
       projects: [],
       products: []
     }
   },
-  created: function(){
+  mounted() {
+  },
+  created: function() {
+    this.$http.get("wc/v2/products/categories?consumer_key=ck_7f3cbd2d927fb455c27fc2efafec0cfb33cf32f3&consumer_secret=cs_4463daf8e892627a8a95ea1d4a40c77d9f4ec74d&per_page=99").then(response => {
+      for (let product in response.data) {
+        this.products.push(response.data[product]);
+      }
+    }, error => {
+      console.error(error);
+    });
+
     this.$http.get('ww/menu').then(response => {
-      console.log(response);
-      for(let project in response.data){
+      for (let project in response.data) {
         this.projects.push(response.data[project]);
       }
     }, error => {
-      alert(error);
+      console.error(error);
     });
   }
 }
@@ -32,17 +44,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -4,22 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
-import WooCommerceAPI from 'woocommerce-api'
 
 Vue.use(VueResource)
-// Vue.use(WooCommerceAPI)
-
-const WooCommerce = new WooCommerceAPI({
-  url: 'https://staging.rextrophies.com',
-  consumerKey: 'ck_7f3cbd2d927fb455c27fc2efafec0cfb33cf32f3',
-  consumerSecret: 'cs_4463daf8e892627a8a95ea1d4a40c77d9f4ec74d',
-  wpAPI: true,
-  version: 'wc/v2',
-  queryStringAuth: true
-});
-WooCommerce.get('products/categories', function(err, data, res) {
-  console.log(res);
-});
 
 Vue.http.options.root = 'https://staging.rextrophies.com/wp-json'
 
